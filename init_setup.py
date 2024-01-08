06898ddc432adfdf896c21b8e27294df67641392
星野有希 <hoshinoyuki@lolicon.io>
2024-01-09 00:30:06 +0800

    Update init_setup.py

--- init_setup.py
+++ init_setup.py
@@ -3,5 +3,15 @@ import os
 # This script will install all the packages required for this project one by one
 # RUN THIS SCRIPT FIRST BEFORE executing "pip install -r requirements.txt"
 
-os.system("pip install discord.py[voice]")
-os.system("pip install py-cord")
+if os.name == 'nt':
+    # For Windows
+    pip_name = "pip"
+elif os.name == 'posix':
+    # For Mac/Linux/BSD
+    pip_name = "pip3"
+else:
+    raise("Runtime error: Unsupported OS")
+    exit(1)
+
+os.system(f"{pip_name} install discord.py[voice]")
+os.system(f"{pip_name} install py-cord")
