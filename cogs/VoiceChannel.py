5be08fabdff716da04d93cfc4e93620e50aecf70
星野ゆき <hoshinoyuki@lolicon.io>
2024-01-11 03:23:05 +0800

    Updated VoiceChannel.py

--- cogs/VoiceChannel.py
+++ cogs/VoiceChannel.py
@@ -1,5 +1,5 @@
 import discord
-from discord import SlashCommandGroup, Interaction, Option
+from discord import SlashCommandGroup, Interaction, Option, FFmpegPCMAudio
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions
 
@@ -21,13 +21,15 @@ class VoiceChannel(commands.Cog):
                 author_vc = channel
             else:
                 author_vc = interaction.author.voice.channel
-            voice = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
+            voice_state = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
             # This allows for more functionality with voice channels
-            if voice == None:
+            if voice_state == None:
                 # None being the default value if the bot isnt in a channel (which is why the is_connected() is returning errors)
                 # Connect the bot to voice channel
-                await author_vc.connect()
+                voice = await author_vc.connect()
                 await interaction.response.send_message(f"I joined the voice channel <#{author_vc.id}>")
+                # #source = FFmpegPCMAudio("test.mp3")
+                # #player = voice.play(source)
             elif voice.channel.id != author_vc.id:
                 # The bot has been connected to a voice channel but not as same as the author or required one
                 if channel is not None:
