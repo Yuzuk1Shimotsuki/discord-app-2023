752a6fbcf51cb25f3ecd2cc59234d94a0c2a488e
星野ゆき <hoshinoyuki@lolicon.io>
2024-05-12 21:54:08 +0800

    Updated Changelog.md

--- startup.py
+++ startup.py
@@ -168,8 +168,9 @@ async def shutdown(ctx):
     if await bot.is_owner(ctx.author):
         bot.clear()
         await bot.close()
+        logger.info("Terminating the application...")
         # Shut down
-        os.kill(os.getpid(), signal.SIGINT) # Terminates the current application
+        exit() # Terminates the current application
     else:
         await ctx.reply(NotBotOwnerError())
 
@@ -180,9 +181,10 @@ async def restart(ctx):
     if await bot.is_owner(ctx.author):
         bot.clear()
         await bot.close()
+        logger.info("Restarting the application...")
         # Restart
         subprocess.run(["python", "restarter.py"])  # Activating restart script
-        os.kill(os.getpid(), signal.SIGINT) # Terminates the current application
+        exit() # Terminates the current application
     else:
         await ctx.reply(NotBotOwnerError())
 
@@ -213,7 +215,7 @@ async def before_serving():
             logger.error("\nThe Discord servers denied the connection for making too many requests, restarting in 7 seconds...")
             logger.error("\nIf the restart fails, get help from 'https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests'")
             subprocess.run(["python", "restarter.py"])
-            os.kill(os.getpid(), signal.SIGINT)
+            exit()
         else:
             raise http_error
     except discord.errors.LoginFailure as token_error:
