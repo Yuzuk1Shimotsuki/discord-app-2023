801b975a00665fb9b21a5b15872e5e66a81ba0da
星野有希 <hoshinoyuki@lolicon.io>
2024-06-14 01:31:01 +0800

    Optimized all fallback messages for moderation section

--- cogs/Timeout.py
+++ cogs/Timeout.py
@@ -16,14 +16,14 @@ class Timeout(commands.Cog):
     async def timeout_member(self, interaction: Interaction, member, days, hours, minutes, seconds, reason):
         duration = timedelta(days = days, hours = hours, minutes = minutes, seconds = seconds)
         if duration >= timedelta(days = 28): #added to check if time exceeds 28 days
-            await interaction.response.send_message("I can't timeout someone for more than 28 days!", ephemeral = True) #responds, but only the author can see the response
+            await interaction.response.send_message("I can't **timeout** someone for **more than 28 days**!", ephemeral = True) #responds, but only the author can see the response
             return
         if reason == None:
             await member.timeout(duration)
-            await interaction.response.send_message(f"<@{member.id}> has been timed out for {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds by <@{interaction.user.id}>.")
+            await interaction.response.send_message(f"<@{member.id}> has been **timed out** for **{days}** days, **{hours}** hours, **{minutes}** minutes, and **{seconds}** seconds.")
         else:
             await member.timeout(duration, reason=reason)
-            await interaction.response.send_message(f"<@{member.id}> has been timed out for {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds by <@{interaction.user.id}> for '{reason}'.")
+            await interaction.response.send_message(f"<@{member.id}> has been **timed out** for **{days}** days, **{hours}** hours, **{minutes}** minutes, and **{seconds}** seconds by for **'{reason}'**.")
 
     # Timeouts a member for a specified amount of time
     @app_commands.command(name="timeout", description="Timeouts a member")
@@ -32,22 +32,22 @@ class Timeout(commands.Cog):
     @app_commands.describe(reason="Reason for timeout")
     async def timeout(self, interaction: Interaction, member: discord.Member, reason: Optional[str] = None, days: Optional[app_commands.Range[int, 0]] = 0, hours: Optional[app_commands.Range[int, None, 23]] = 0, minutes: Optional[app_commands.Range[int, None, 59]] = 0, seconds: Optional[app_commands.Range[int, None, 59]] = 0):  # setting each value with a default value of 0 reduces a lot of the code
         if member.id == interaction.user.id:
-            return await interaction.response.send_message("BRUH! You can't timeout yourself!")
+            return await interaction.response.send_message("BRUH! You can't **timeout yourself**!")
         elif member.id == self.bot.application_id:
             # To prevent the bot bans itself from the server by accident
-            await interaction.response.send_message(f"i cannot just timeout myself ^u^")
+            await interaction.response.send_message(f"i cannot just **timeout myself** ^u^")
         elif member.guild_permissions.administrator:
             if interaction.user.id == interaction.guild.owner.id:
                 await self.timeout_member(interaction, member, days, hours, minutes, seconds, reason)
             else:
-                return await interaction.response.send_message("You can't do this, this person is a moderator!")
+                return await interaction.response.send_message("You can't do this, this person is a **moderator**!")
         else:
             await self.timeout_member(interaction, member, days, hours, minutes, seconds, reason)
 
     @timeout.error
     async def timeouterror(self, interaction: Interaction, error):
         if isinstance(error, MissingPermissions):
-            await interaction.response.send_message("You cannot do this without moderate members permissions!")
+            await interaction.response.send_message("You cannot do this **without moderate members permissions**!")
         else:
             raise error
 
