b5cde1a6ebd7adb8a54f58d2f18ded0697a13167
星野ゆき <hoshinoyuki@lolicon.io>
2024-06-16 02:56:45 +0800

    Seperated all cogs by its category

--- startup.py
+++ startup.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 load_dotenv()
 nest_asyncio.apply()
 app = Quart("DiscordBot")
+extensions = []
+extensions_folders = ['cogs', 'moderation']
 
 logger = logging.getLogger(__name__)
 ConsoleOutputHandler = logging.StreamHandler()
@@ -31,10 +33,30 @@ class Bot(commands.Bot):
         super().__init__(
             intents=intents, 
             command_prefix="!",
-            self_bot=False, strip_after_prefix = True
+            self_bot=False,     # This is IMPORTANT!
+            strip_after_prefix = True
         )
 
+class MyNewHelp(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        for page in self.paginator.pages:
+            emby = discord.Embed(description=page)
+            await destination.send(embed=emby)
+
 bot = Bot()
+bot.help_command = MyNewHelp()
+
+# Getting all extensions
+def get_extensions():
+    global extensions
+    global extensions_folders
+    extensions = []
+    for folder in extensions_folders:
+        for filename in os.listdir(f"./{folder}"):
+            if filename.endswith('.py'):
+                extensions.append(f'{folder}.{filename[:-3]}')
+    return extensions
 
 # Custom errors
 class NotBotOwnerError:
@@ -45,21 +67,21 @@ class ExtensionNotFoundError:
     def __init__(self, cog: str) -> None:
         self.cog = cog
 
-    def return_msg(self) -> str:
+    def __repr__(self) -> str:
         return f"I can't found the cog `{self.cog}` :pensive_face: ... Perhaps it was not a vaild input :thinking: ？"
 
 class ReturnNoEntryPointError:
     def __init__(self, cog: str) -> None:
         self.cog = cog
 
-    def return_msg(self) -> str:
+    def __repr__(self) -> str:
         return f"I can't found the `async def setup()` function in cog `{self.cog}` :pensive_face: ... Perhaps check the cog and try again :thinking: ？"
-    
+
 class ExtensionFailedError:
     def __init__(self, cog: str) -> None:
         self.cog = cog
 
-    def return_msg(self) -> str:
+    def __repr__(self) -> str:
         return f"Some unexpected stuff happened while executing `{self.cog}`."
 
 
@@ -75,150 +97,146 @@ async def on_ready():
     logger.info("-" * 140)
 
 # Sync all cogs for latest changes
-@bot.command() 
+@bot.command()
 async def sync(ctx):
-    if await bot.is_owner(ctx.author):
-        synced = await bot.tree.sync()
-        msg = await ctx.reply(f"Synced {len(synced)} command(s).")
-        await asyncio.sleep(5)
-        await msg.delete()
-        await ctx.message.delete()
-    else:
-        await ctx.reply(NotBotOwnerError())
+    if not await bot.is_owner(ctx.author):
+        return await ctx.reply(NotBotOwnerError())
+    synced = await bot.tree.sync()
+    msg = await ctx.reply(f"Synced {len(synced)} command(s).")
+    await asyncio.sleep(5)
+    await msg.delete()
+    await ctx.message.delete()
 
 # Load cogs manually
 @bot.command()
 async def load(ctx, cog_name):
-    if await bot.is_owner(ctx.author):
-        # Front check if the cog was in the valid cog list or not
-        if f"{cog_name}.py" not in os.listdir("./cogs"):
-            return await ctx.reply(ExtensionNotFoundError(cog=cog_name).return_msg())
-        try:
-            await bot.load_extension(f"cogs.{cog_name}")
-            await bot.tree.sync()
-            msg = await ctx.reply(f"Cog `{cog_name}` has been loaded.")
-            await asyncio.sleep(1)
-            await msg.delete()
-            await ctx.message.delete()
-        except ExtensionAlreadyLoaded:
-            return await ctx.reply(f"Cog `{cog_name}` is already loaded.")
-        except NoEntryPointError:
-            return await ctx.reply(ReturnNoEntryPointError(cog=cog_name).return_msg())
-        except ExtensionFailed:
-            return await ctx.reply(ExtensionFailedError(cog=cog_name).return_msg())
-    else:
-        await ctx.reply(NotBotOwnerError())
+    if not await bot.is_owner(ctx.author):
+        return await ctx.reply(NotBotOwnerError())
+    # Front check if the cog was in the valid cog list or not
+    extensions = get_extensions()
+    if cog_name not in extensions:
+        return await ctx.reply(ExtensionNotFoundError(cog=cog_name))
+    try:
+        await bot.load_extension(cog_name)
+        await bot.tree.sync()
+        msg = await ctx.reply(f"Cog `{cog_name}` has been loaded.")
+        await asyncio.sleep(1)
+        await msg.delete()
+        await ctx.message.delete()
+    except ExtensionAlreadyLoaded:
+        return await ctx.reply(f"Cog `{cog_name}` has been already loaded!")
+    except NoEntryPointError:
+        return await ctx.reply(ReturnNoEntryPointError(cog=cog_name))
+    except ExtensionFailed:
+        return await ctx.reply(ExtensionFailedError(cog=cog_name))
 
 # Unload cogs manually
 @bot.command()
 async def unload(ctx, cog_name):
-    if await bot.is_owner(ctx.author):
-        if f"{cog_name}.py" not in os.listdir("./cogs"):
-            # Front check if the cog was in the valid cog list or not
-            return await ctx.reply(ExtensionNotFoundError(cog=cog_name).return_msg())
-        try:
-            await bot.unload_extension(f"cogs.{cog_name}")
-            await bot.tree.sync()
-            msg = await ctx.reply(f"Cog `{cog_name}` has been unloaded.")
-            await asyncio.sleep(2)
-            await msg.delete()
-            await ctx.message.delete()
-        except ExtensionNotLoaded:
-            return await ctx.reply(f"Cog `{cog_name}` is already unloaded.")
-        except NoEntryPointError:
-            return await ctx.reply(ReturnNoEntryPointError(cog=cog_name).return_msg())
-        except ExtensionFailed:
-            return await ctx.reply(ExtensionFailedError(cog=cog_name).return_msg())
-    else:
-        await ctx.reply(NotBotOwnerError())
+    if not await bot.is_owner(ctx.author):
+        return await ctx.reply(NotBotOwnerError())
+    extensions = get_extensions()
+    if cog_name not in extensions:
+        # Front check if the cog was in the valid cog list or not
+        return await ctx.reply(ExtensionNotFoundError(cog=cog_name))
+    try:
+        await bot.unload_extension(cog_name)
+        await bot.tree.sync()
+        msg = await ctx.reply(f"Cog `{cog_name}` has been unloaded.")
+        await asyncio.sleep(2)
+        await msg.delete()
+        await ctx.message.delete()
+    except ExtensionNotLoaded:
+        return await ctx.reply(f"Cog `{cog_name}` has been already unloaded!")
+    except NoEntryPointError:
+        return await ctx.reply(ReturnNoEntryPointError(cog=cog_name))
+    except ExtensionFailed:
+        return await ctx.reply(ExtensionFailedError(cog=cog_name))
 
 # Reload cogs manually
 @bot.command()
 async def reload(ctx, cog_name):
-    if await bot.is_owner(ctx.author):
-        # Front check if the cog was in the valid cog list or not
-        if f"{cog_name}.py" not in os.listdir("./cogs"):
-            return await ctx.reply(ExtensionNotFoundError(cog=cog_name).return_msg())
-        try:
-            await bot.reload_extension(cog_name)
-            await bot.tree.sync()
-            msg = await ctx.reply(f"Cog `{cog_name}` has been reloaded.")
-            await asyncio.sleep(2)
-            await msg.delete()
-            await ctx.message.delete()
-        except ExtensionNotLoaded:
-            return await ctx.send(f"Cog `{cog_name}` has not been loaded.")
-        except NoEntryPointError:
-            return await ctx.reply(ReturnNoEntryPointError(cog=cog_name).return_msg())
-        except ExtensionFailed:
-            return await ctx.reply(ExtensionFailedError(cog=cog_name).return_msg())
-    else:
-        await ctx.reply(NotBotOwnerError())
+    if not await bot.is_owner(ctx.author):
+        return await ctx.reply(NotBotOwnerError())
+    extensions = get_extensions()
+    if cog_name not in extensions:
+        return await ctx.reply(ExtensionNotFoundError(cog=cog_name))
+    try:
+        await bot.reload_extension(cog_name)
+        await bot.tree.sync()
+        msg = await ctx.reply(f"Cog `{cog_name}` has been reloaded.")
+        await asyncio.sleep(2)
+        await msg.delete()
+        await ctx.message.delete()
+    except ExtensionNotLoaded:
+        return await ctx.send(f"Cog `{cog_name}` has not been loaded.")
+    except NoEntryPointError:
+        return await ctx.reply(ReturnNoEntryPointError(cog=cog_name))
+    except ExtensionFailed:
+        return await ctx.reply(ExtensionFailedError(cog=cog_name))
 
 # Retrieving system info from the bot
 @bot.command()
 async def systeminfo(ctx):
-    if await bot.is_owner(ctx.author):
-        # CPU
-        cpuPercentage = psutil.cpu_percent(interval=1, percpu=False) * 100
-        numberOfSystemCores = psutil.cpu_count(logical=False)
-        numberOfLogicalCores = psutil.cpu_count(logical=True)
-        # Memory
-        ram = psutil.virtual_memory()
-        usedRamInGB = round(ram.used / 1024 ** 3, 2)
-        freeRamInGB = round(ram.free / 1024 ** 3, 2)
-        totalRamInGB = round(ram.total / 1024 ** 3, 2)
-        ramPercentage = ram.percent
-        # Storage
-        disk = psutil.disk_usage('/')
-        usedVolumeInGB = round(disk.used / 1024 ** 3, 2)
-        freeVolumeInGB = round(disk.free / 1024 ** 3, 2)
-        totalVolumeInGB = round(disk.total / 1024 ** 3, 2)
-        diskPercentage = disk.percent
-        # Network
-        network = psutil.net_io_counters()
-        # Returning system info as embed
-        hardware_info_embed = discord.Embed(title="Resource Usage (For reference only):", description='\u200b', timestamp=datetime.now(), color=ctx.author.colour)
-        hardware_info_embed.add_field(name="CPU", value=f"CPU utilization: {cpuPercentage}%\nNumber of system cores: {numberOfSystemCores}\nNumber of logical cores: {numberOfLogicalCores}", inline=False)
-        hardware_info_embed.add_field(name="\u200b", value="", inline=False)
-        hardware_info_embed.add_field(name="RAM", value=f"Memory in use: {usedRamInGB} / {totalRamInGB} GB ({ramPercentage}%)\nAvailible memory: {freeRamInGB} GB", inline=False)
-        hardware_info_embed.add_field(name="\u200b", value="", inline=False)
-        hardware_info_embed.add_field(name="Storage", value=f"Space used: {usedVolumeInGB} / {totalVolumeInGB} GB ({diskPercentage}%)\nAvailible space: {freeVolumeInGB} GB", inline=False)
-        hardware_info_embed.add_field(name="\u200b", value="", inline=False)
-        hardware_info_embed.add_field(name="Network", value=f"Number of bytes sent: {network.bytes_sent}\nNumber of bytes received: {network.bytes_recv}\nNumber of packets sent: {network.packets_sent}\nNumber of packets received: {network.packets_recv}\nTotal number of errors while receiving: {network.errin}\nTotal number of errors while sending: {network.errout}\nTotal number of incoming packets dropped: {network.dropin}\nTotal number of outgoing packets dropped: {network.dropout}", inline=False)
-        hardware_info_embed.add_field(name="\u200b", value="", inline=False)
-        await ctx.reply(embed=hardware_info_embed)
-    else:
-        await ctx.reply(NotBotOwnerError())
+    if not await bot.is_owner(ctx.author):
+        return await ctx.reply(NotBotOwnerError())
+    # CPU
+    cpuPercentage = psutil.cpu_percent(interval=1, percpu=False) * 100
+    numberOfSystemCores = psutil.cpu_count(logical=False)
+    numberOfLogicalCores = psutil.cpu_count(logical=True)
+    # Memory
+    ram = psutil.virtual_memory()
+    usedRamInGB = round(ram.used / 1024 ** 3, 2)
+    freeRamInGB = round(ram.free / 1024 ** 3, 2)
+    totalRamInGB = round(ram.total / 1024 ** 3, 2)
+    ramPercentage = ram.percent
+    # Storage
+    disk = psutil.disk_usage('/')
+    usedVolumeInGB = round(disk.used / 1024 ** 3, 2)
+    freeVolumeInGB = round(disk.free / 1024 ** 3, 2)
+    totalVolumeInGB = round(disk.total / 1024 ** 3, 2)
+    diskPercentage = disk.percent
+    # Network
+    network = psutil.net_io_counters()
+    # Returning system info as embed
+    hardware_info_embed = discord.Embed(title="Resource Usage (For reference only):", description='\u200b', timestamp=datetime.now(), color=ctx.author.colour)
+    hardware_info_embed.add_field(name="CPU", value=f"CPU utilization: {cpuPercentage}%\nNumber of system cores: {numberOfSystemCores}\nNumber of logical cores: {numberOfLogicalCores}", inline=False)
+    hardware_info_embed.add_field(name="\u200b", value="", inline=False)
+    hardware_info_embed.add_field(name="RAM", value=f"Memory in use: {usedRamInGB} / {totalRamInGB} GB ({ramPercentage}%)\nAvailible memory: {freeRamInGB} GB", inline=False)
+    hardware_info_embed.add_field(name="\u200b", value="", inline=False)
+    hardware_info_embed.add_field(name="Storage", value=f"Space used: {usedVolumeInGB} / {totalVolumeInGB} GB ({diskPercentage}%)\nAvailible space: {freeVolumeInGB} GB", inline=False)
+    hardware_info_embed.add_field(name="\u200b", value="", inline=False)
+    hardware_info_embed.add_field(name="Network", value=f"Number of bytes sent: {network.bytes_sent}\nNumber of bytes received: {network.bytes_recv}\nNumber of packets sent: {network.packets_sent}\nNumber of packets received: {network.packets_recv}\nTotal number of errors while receiving: {network.errin}\nTotal number of errors while sending: {network.errout}\nTotal number of incoming packets dropped: {network.dropin}\nTotal number of outgoing packets dropped: {network.dropout}", inline=False)
+    hardware_info_embed.add_field(name="\u200b", value="", inline=False)
+    await ctx.reply(embed=hardware_info_embed)
 
 # Restart the bot (Use it only as a LAST RESORT)
 @bot.command()
 async def restart(ctx):
-    if await bot.is_owner(ctx.author):
-        bot.clear()
-        await bot.close()
-        # Restart
-        subprocess.Popen([sys.executable, *sys.argv])
-    else:
-        await ctx.reply(NotBotOwnerError())
+    if not await bot.is_owner(ctx.author):
+        return await ctx.reply(NotBotOwnerError())
+    bot.clear()
+    await bot.close()
+    # Restart
+    subprocess.Popen([sys.executable, *sys.argv])
 
 # Shut down the bot (SELF DESTRUCT)
 @bot.command()
 async def shutdown(ctx):
-    if await bot.is_owner(ctx.author):
-        bot.clear()
-        await bot.close()
-        exit(0)
-    else:
-        await ctx.reply(NotBotOwnerError())
+    if not await bot.is_owner(ctx.author):
+        return await ctx.reply(NotBotOwnerError())
+    bot.clear()
+    await bot.close()
+    exit(0)
 
 # Load extensions
 async def load_extensions():
+    logger.info("\nGetting extensions...\n")
+    initial_extensions = get_extensions()
     logger.info("\nLoading extensions...\n")
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{filename[:-3]}')
-            logger.info(f'cogs.{filename[:-3]}')\
+    for extension in initial_extensions:
+        await bot.load_extension(extension)
+        logger.info(extension)
 
 # Starting the bot
 @app.before_serving
