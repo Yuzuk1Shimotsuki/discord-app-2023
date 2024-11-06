6fda5fbd3f0128eb47f50bd48d49d1b21efdb5a5
星野ゆき <hoshinoyuki@lolicon.io>
2024-11-06 21:55:30 +0800

    Rewrited and refactored most of the codes in moderation session

--- moderation/Untimeout.py
+++ moderation/Untimeout.py
@@ -1,6 +1,7 @@
 import discord
-from discord import app_commands, Interaction
+from discord import app_commands, Embed, Interaction
 from discord.ext import commands
+from discord.app_commands import BotMissingPermissions
 from discord.ext.commands import MissingPermissions
 from typing import Optional
 
@@ -17,17 +18,24 @@ class Untimeout(commands.Cog):
     @app_commands.describe(member="Member to untimeout (Enter the User ID e.g. 529872483195806124)")
     @app_commands.describe(reason="Reason for untimeout")
     async def untimeout(self, interaction: Interaction, member: discord.Member, reason: Optional[str] = None):
-        if reason == None:
-            await member.timeout(None)
-            await interaction.response.send_message(f"<@{member.id}> has been **untimed out**.")
-        else:
+        untimeout_embed = Embed(title="", color=interaction.user.color)
+        if reason is not None:
             await member.timeout(None, reason=reason)
-            await interaction.response.send_message(f"<@{member.id}> has been **untimed out**. Reason: **{reason}**.")
+            untimeout_embed.add_field(name="", value=f"{member.mention} has been **untimeout**.\nReason: **{reason}**.")
+        else:
+            await member.timeout(None)
+            untimeout_embed.add_field(name="", value=f"{member.mention} has been **untimeout**.")
+        await interaction.response.send_message(embed=untimeout_embed)
 
     @untimeout.error
     async def untimeout_error(self, interaction: Interaction, error):
+        untimeout_error_embed = Embed(title="", color=discord.Colour.red())
         if isinstance(error, MissingPermissions):
-            await interaction.response.send_message("You cannot do this **without moderate members permissions**!")
+            untimeout_error_embed.add_field(name="", value=f"<a:CrossRed:1274034371724312646> This command **requires** `moderate members` permission, and you probably **don't have** it, {interaction.user.mention}.")
+            await interaction.response.send_message(embed=untimeout_error_embed)
+        elif isinstance(error, BotMissingPermissions):
+            untimeout_error_embed.add_field(name="", value=f"<a:CrossRed:1274034371724312646> I couldn't **untimeout** that user. Please **double-check** my **permissions** and **role position**.")
+            await interaction.response.send_message(embed=untimeout_error_embed)
         else:
             raise error
 
