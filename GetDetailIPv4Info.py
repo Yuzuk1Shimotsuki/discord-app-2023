53917e17b358fe397b337c33515d1c7968242dc7
星野ゆき <hoshinoyuki@lolicon.io>
2026-01-25 02:11:32 +0800

    chore: Update license info on some files

--- GetDetailIPv4Info.py
+++ GetDetailIPv4Info.py
@@ -2,6 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2024 ChocolaMilk92
+Copyright (c) 2025-present shirakamiNeko
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
